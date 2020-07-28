8649251911da069b9a51ba29f13aaa6a14ed624d
Jake McGinty <me@jake.su>
2020-07-28 17:39:00 +0900

    break out protocol structs into crate

--- src/protocol.rs
+++ src/protocol.rs
@@ -7,6 +7,7 @@ use hal::{
     stm32,
 };
 use nb::{self, block};
+pub use panel_protocol::{Command, Report};
 
 #[derive(Debug)]
 pub enum Error {
@@ -24,107 +25,6 @@ impl From<serial::Error> for Error {
 impl From<arrayvec::CapacityError> for Error {
     fn from(_: arrayvec::CapacityError) -> Error {
         Error::BufferFull
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub enum Command {
-    PowerCycler { slot: u8, state: bool },
-    Brightness { value: u16 },
-    Temperature { value: u16 },
-}
-
-impl Command {
-    pub fn try_from(buf: &[u8]) -> Result<Option<(Command, usize)>, ()> {
-        if buf.is_empty() {
-            return Ok(None);
-        }
-
-        match *buf {
-            [] => Ok(None),
-            [b'A', slot, state, ..] => Ok(Some((Command::PowerCycler { slot, state: state != 0 }, 3))),
-            [b'B', msb, lsb, ..] => {
-                let value = u16::from_be_bytes([msb, lsb]);
-                Ok(Some((Command::Brightness { value }, 3)))
-            },
-            [b'C', msb, lsb, ..] => {
-                let value = u16::from_be_bytes([msb, lsb]);
-                Ok(Some((Command::Temperature { value }, 3)))
-            },
-            _ => Err(()),
-        }
-    }
-
-    pub fn as_arrayvec(&self) -> ArrayVec<[u8; 8]> {
-        let mut buf = ArrayVec::new();
-        match *self {
-            Command::PowerCycler { slot, state } => {
-                buf.push(b'A');
-                buf.push(slot);
-                buf.push(u8::from(state));
-            },
-            Command::Brightness { value } => {
-                buf.push(b'B');
-                buf.try_extend_from_slice(&value.to_be_bytes()).unwrap();
-            },
-            Command::Temperature { value } => {
-                buf.push(b'C');
-                buf.try_extend_from_slice(&value.to_be_bytes()).unwrap();
-            },
-        }
-        buf
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub enum Report {
-    DialValue { diff: i8 },
-    Click,
-    EmergencyOff,
-    Error { code: u16 },
-}
-
-impl Report {
-    pub fn try_from(buf: &[u8]) -> Result<Option<(Report, usize)>, ()> {
-        if buf.is_empty() {
-            return Ok(None);
-        }
-
-        match *buf {
-            [] => Ok(None),
-            [b'A', diff, ..] => {
-                let diff = i8::from_be_bytes([diff]);
-                Ok(Some((Report::DialValue { diff }, 2)))
-            },
-            [b'B', ..] => Ok(Some((Report::Click, 1))),
-            [b'C', ..] => Ok(Some((Report::EmergencyOff, 1))),
-            [b'D', msb, lsb, ..] => {
-                let code = u16::from_be_bytes([msb, lsb]);
-                Ok(Some((Report::Error { code }, 3)))
-            },
-            _ => Err(()),
-        }
-    }
-
-    pub fn as_arrayvec(&self) -> ArrayVec<[u8; 8]> {
-        let mut buf = ArrayVec::new();
-        match *self {
-            Report::DialValue { diff } => {
-                buf.push(b'A');
-                buf.try_extend_from_slice(&diff.to_be_bytes()).unwrap();
-            },
-            Report::Click => {
-                buf.push(b'B');
-            },
-            Report::EmergencyOff => {
-                buf.push(b'C');
-            },
-            Report::Error { code } => {
-                buf.push(b'D');
-                buf.try_extend_from_slice(&code.to_be_bytes()).unwrap();
-            },
-        }
-        buf
     }
 }
 
