e88e6e5e5abbda8da137ec4ebaece33159d83f99
Brian Schwind <brianmschwind@gmail.com>
2020-11-19 15:02:14 +0900

    Use version 0.2 of panel-protocol, make the LED pulse a bit faster

--- src/main.rs
+++ src/main.rs
@@ -115,7 +115,7 @@ fn main() -> ! {
     let mut led_strip = LedStrip::new(spi);
 
     let timer = MonoTimer::new(cp.DWT, cp.DCB, clocks);
-    let pulser = Pulser::new(5000, &timer);
+    let pulser = Pulser::new(700, &timer);
 
     // PWM Setup
     // https://docs.rs/stm32f1xx-hal/0.6.1/stm32f1xx_hal/timer/index.html
