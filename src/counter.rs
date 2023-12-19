5eb98f0c4e1d7202d82fea7d5ed8381f02797332
Brian Schwind <brianmschwind@gmail.com>
2023-12-19 11:54:34 +0900

    Specify QEI Mode (#33)
    * Change rotary encoder polarity to falling edge
    
    * Fix preexisting clippy warnings
    
    * Working version of QEI decoder without backlash
    
    * Move the encoder mode register write to counter.rs, document
    
    ---------
    
    Co-authored-by: Jen Tak <goodhoko@gmail.com>

--- src/counter.rs
+++ src/counter.rs
@@ -8,6 +8,16 @@ pub struct Counter<PINS> {
 
 impl<PINS> Counter<PINS> {
     pub fn new(qei: Qei<TIM1, PINS>) -> Self {
+        unsafe {
+            // TODO(bschwind) - Expose this functionality with a safe interface
+            //                  in stm32f4xx-hal.
+            // Change the mode of the QEI decoder to mode 1:
+            // Counter counts up/down on TI2FP1 edge depending on TI1FP2 level.
+            // Or in layman's terms, the encoder counts up and down on encoder
+            // pin A edges, while referencing the state of encoder pin B.
+            (*TIM1::ptr()).smcr.write(|w| w.sms().encoder_mode_1());
+        }
+
         let last_count = qei.count();
         Counter { qei, last_count }
     }
@@ -16,9 +26,9 @@ impl<PINS> Counter<PINS> {
         let count = self.qei.count();
         let diff = count.wrapping_sub(self.last_count) as i16;
 
-        if diff.abs() >= 4 {
+        if diff.abs() >= 2 {
             self.last_count = count;
-            Some((diff / 4) as i8)
+            Some((diff / 2) as i8)
         } else {
             None
         }
