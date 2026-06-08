b614f93b59ba14da872a77e21022232b22e7c547
Brian Schwind <brianmschwind@gmail.com>
2026-06-08 18:30:22 +0900

    Simplify the rotary encoder logic

--- src/counter.rs
+++ src/counter.rs
@@ -1,59 +1,31 @@
 use core::convert::Infallible;
 use embedded_hal::digital::v2::InputPin;
 
-// How many up/down edges occur on either the A or B pins from one
-// physical detent to the next. Most encoders have 4 edges, but
-// ours has 2, and others could have a different number.
-const EDGES_PER_DETENT: i8 = 4;
-
 pub struct Counter<A: InputPin, B: InputPin> {
-    // qei: Qei<TIM1, PINS>,
     pin_a: A,
     pin_b: B,
     prev_state: (bool, bool),
-    pulse_count: i8,
 }
 
-impl<A: InputPin<Error=Infallible>, B: InputPin<Error=Infallible>> Counter<A, B> {
+impl<A: InputPin<Error = Infallible>, B: InputPin<Error = Infallible>> Counter<A, B> {
     pub fn new(pin_a: A, pin_b: B) -> Self {
         let prev_state = read_state(&pin_a, &pin_b);
 
-        Self {
-            pin_a,
-            pin_b,
-            prev_state,
-            pulse_count: 0,
-        }
+        Self { pin_a, pin_b, prev_state }
     }
 
     pub fn poll(&mut self) -> Option<i8> {
-        let mut dial_diff = None;
-
         let curr_state = read_state(&self.pin_a, &self.pin_b);
 
-        // Valid transitions
-        // Clockwise:
-        //     00 -> 10 -> 11 -> 01 -> 00
-        // Counter-clockwise:
-        //     00 -> 01 -> 11 -> 10 -> 00
-        match (self.prev_state, curr_state) {
-            ((false, false), (false, true))
-            | ((false, true), (true, true))
-            | ((true, true), (true, false))
-            | ((true, false), (false, false)) => self.pulse_count -= 1,
-            ((false, false), (true, false))
-            | ((true, false), (true, true))
-            | ((true, true), (false, true))
-            | ((false, true), (false, false)) => self.pulse_count += 1,
-            _ => {},
-        }
-
-        if self.pulse_count.abs() >= EDGES_PER_DETENT {
-            // Will be -1 or +1
-            let diff = self.pulse_count.signum();
-            self.pulse_count = 0;
-            dial_diff = Some(diff);
-        }
+        // We only count a pulse on the rising or falling edge of B, and
+        // the direction depends on the level of A, which should be unchanged
+        // as B rises or falls. The falling edge of B should be roughly in the
+        // middle of the two detents, according to the Alps EC20A datasheet.
+        let dial_diff = match (self.prev_state, curr_state) {
+            ((false, true), (false, false)) => Some(1),
+            ((false, false), (false, true)) => Some(-1),
+            _ => None,
+        };
 
         self.prev_state = curr_state;
 
@@ -61,6 +33,9 @@ impl<A: InputPin<Error=Infallible>, B: InputPin<Error=Infallible>> Counter<A, B>
     }
 }
 
-fn read_state<A: InputPin<Error=Infallible>, B: InputPin<Error=Infallible>>(a: &A, b: &B) -> (bool, bool) {
+fn read_state(
+    a: &dyn InputPin<Error = Infallible>,
+    b: &dyn InputPin<Error = Infallible>,
+) -> (bool, bool) {
     (a.is_high().unwrap(), b.is_high().unwrap())
 }
